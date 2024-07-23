475c9ac6aaf17c8fa3054c6c537a8f0b05dd3920
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2024-07-23 01:26:32 -0300

    feat: adding logic for card usage by customers

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,16 +1,44 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Cliente {
+    private String nome;
+    private List<Cartao> cartoes;
 
-	private String nome;
-	private List<Cartao> cartoes;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
+	public Cliente(String nome){
 		this.nome = nome;
+		this.cartoes = new ArrayList<>();
 	}
 
+    public Cliente() {
+        this.cartoes = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Cartao> getCartoes() {
+        return cartoes;
+    }
+
+    public void adicionarCartao(Cartao cartao) {
+        this.cartoes.add(cartao);
+    }
+
+    public void realizarPagamento(String numeroCartao, double valor) {
+		Optional<Cartao> cartaoParaPagamento = cartoes.stream()
+			.filter(cartao -> cartao.getNumero().equals(numeroCartao))
+			.findFirst();
+
+		cartaoParaPagamento.ifPresentOrElse(
+			cartao -> cartao.pagar(valor),
+			() -> {System.out.println("Cartão não encontrado");}
+		);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,25 @@ public class Main {
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(100);
+		cc.depositar(500);
 		cc.transferir(100, poupanca);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
+
+		CartaoDebito cartaoDebito = new CartaoDebito("1111-1111-1111-1111", venilton, cc);
+        CartaoCredito cartaoCredito = new CartaoCredito("2222-2222-2222-2222", venilton, cc, 1000);
+
+        venilton.adicionarCartao(cartaoDebito);
+        venilton.adicionarCartao(cartaoCredito);
+
+        venilton.realizarPagamento("1111-1111-1111-1111", 200); 
+        venilton.realizarPagamento("1111-1111-1111-1111", 900); 
+
+        venilton.realizarPagamento("2222-2222-2222-2222", 300); 
+        venilton.realizarPagamento("2222-2222-2222-2222", 800); 
+
+        venilton.realizarPagamento("2222-2222-2222-2222", 200); 
 	}
 
 }
